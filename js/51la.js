d38eeca1bbe40a6c1ca5c9a1d80f78d9055f86dd
3DOnePro <762219136@qq.com>
2023-01-06 14:11:08 +0800

    Site updated: 2023-01-06 14:11:07

--- js/51la.js
+++ js/51la.js
@@ -1,1 +1,5 @@
 document.write("<script>!function(p){\"use strict\";!function(t){var s=window,e=document,i=p,c=\"\".concat(\"https:\"===e.location.protocol?\"https://\":\"http://\",\"sdk.51.la/js-sdk-pro.min.js\"),n=e.createElement(\"script\"),r=e.getElementsByTagName(\"script\")[0];n.type=\"text/javascript\",n.setAttribute(\"charset\",\"UTF-8\"),n.async=!0,n.src=c,n.id=\"LA_COLLECT\",i.d=n;var o=function(){s.LA.ids.push(i)};s.LA?s.LA.ids&&o():(s.LA=p,s.LA.ids=[],o()),r.parentNode.insertBefore(n,r)}()}({id:\"Jvj3gHXNqjq59MIQ\",ck:\"Jvj3gHXNqjq59MIQ\"});</script>")
+document.write("<script src=\"https:\/\/sdk.51.la\/perf\/js-sdk-perf.min.js\" crossorigin=\"anonymous\"><\/script>");
+document.write("<script>");
+document.write("  new LingQue.Monitor().init({id:\"JvjOLtEmgiITwhH0\"});");
+document.write("<\/script>");
